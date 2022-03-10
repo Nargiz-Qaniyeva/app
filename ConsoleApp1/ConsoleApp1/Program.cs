556b43d868f0043b0dffe87bdad06a2376188b86
Nargiz Ganiyeva <tu7lyw21z@aztu.local>
2022-03-10 12:54:33 +0400

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int[] arr = { 1, 3, 4, 7, -9, 2, -5 };
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] < 0)
+
+                {
+                   arr[i] *=  -1;
+
+                }
+
+                //menfini musbet etdikden sonra, umumi massivi geri qaytara bilmirem(((;
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                System.Console.Write($" {arr[i]}");
+            }
+        }
+
+
+
+    }
+
+
+}
